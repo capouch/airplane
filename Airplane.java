804f5bfe7c4d87e4388ee385d5a70cd4b02d5410
Brian Capouch <brianc@saintjoe.edu>
2015-02-11 15:51:52 -0500

    New files

---
+++ Airplane.java
@@ -0,0 +1,16 @@
+public class Airplane {
+	public String make;
+	public String model;
+	public int year;
+	public int capacity;
+
+/* ----------------------------------------------------------------- */
+
+	Airplane(String thisMake, String thisModel, int thisYear, int thisCapacity) {
+	  make = thisMake;
+	  model = thisModel;
+	  year = thisYear;
+	  capacity = thisCapacity;
+ 	  }
+
+}
---
+++ AirplaneDriver.java
@@ -0,0 +1,6 @@
+public class AirplaneDriver {
+	public static void main(String[] args) {
+	  // Now create an airplane
+	  Airplane myplane = new Airplane("Make","Model",1999,100);
+	}
+	}	
